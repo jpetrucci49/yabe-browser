f35388a551297ea9d22e2ca1ce5474610e954ec0
jpetrucci <jpetrucci49@yahoo.com>
2018-10-17 18:52:18 -0400

    Fix hundredths decimal for price
     - ItemBid.js
      - Add Number(bid + price).toFixed(2) to total

--- src/auctions/components/ItemBid.js
+++ src/auctions/components/ItemBid.js
@@ -28,12 +28,12 @@ class ItemBid extends Component {
     if ( this.state.item.bid <= 0 ) {
       flash('Nice try, you cheeky devil. This isn\'t that kind of auction.', 'flash-error' )
     } else {
-      const newPrice = (Number(this.state.item.bid.toFixed(2)) + Number(Number(this.props.item.price).toFixed(2)))
+      const newPrice = (Number(this.state.item.bid.toFixed(2)) + Number(Number(this.props.item.price).toFixed(2))).toFixed(2)
       const placeBid = JSON.stringify({item: {price: newPrice}})
       const response = await axios.patch(`${apiUrl}/items/${match.params.id}`, placeBid, { 'headers': { 'Authorization': `Bearer ${user.token}` }})
         .then(() => this.props.sendBid(newPrice))
-        .then(() => flash('You have successfully placed a bid!', 'flash-success'))
-        .catch(() => flash('There was an problem with your bid.', 'flash-error'))
+        // .then(() => flash('You have successfully placed a bid!', 'flash-success'))
+        // .catch(() => flash('There was an problem with your bid.', 'flash-error'))
     }
   }
 
